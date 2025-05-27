1be0f14d71428b2c9a1f80d95b59c48467d627ce
jigordev <jigordev@gmail.com>
2025-05-27 11:53:27 -0300

    feat: add response schema

--- mock_api/main.py
+++ mock_api/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 from factory import generate_certidoes
 
 app = FastAPI(root_path="/api-mock")
 
 
-@app.get("/certidoes")
+class Certidao(BaseModel):
+    tipo: str
+    status: str
+    conteudo_base64: str
+
+
+class CertidaoResponse(BaseModel):
+    cpf_cnpj: str
+    certidoes: List[Certidao]
+
+
+@app.get("/certidoes", response_model=CertidaoResponse)
 async def get_certidoes(cpf_cnpj: str):
     certidoes = generate_certidoes()
     return {"cpf_cnpj": cpf_cnpj, "certidoes": certidoes}
